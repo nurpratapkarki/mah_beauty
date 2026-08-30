d428e6739098c40eb85539b727b61e8889593f45
Nur Pratap Karki <nurpratapkarki@gmail.com>
2026-08-30 17:58:21 +0545

    admin/ removed

--- mah_beauty_project/urls.py
+++ mah_beauty_project/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('shop.api_urls')),
 ]
